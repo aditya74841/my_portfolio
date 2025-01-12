ca38877c434a320a1a71fedca075cc77c788253c
Aditya Ranjan <aditya74810@gmail.com>
2025-01-12 17:34:18 +0530

    added analaytcs

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
-import About from './components/about/About'
-import Experience from './components/experience/Experience'
-import Portfolio from './components/portfolio/Portfolio'
-import Services from './components/services/Services'
-import Testimonials from './components/testimonials/Testimonials'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
+import React from "react";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Portfolio from "./components/portfolio/Portfolio";
+import Services from "./components/services/Services";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import { Helmet } from "react-helmet";
 
 const App = () => {
   return (
     <div>
       <>
+        <Helmet>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-79B88YP1GN"
+          ></script>
+          <script>
+            window.dataLayer = window.dataLayer || []; function gtag()
+            {dataLayer.push(arguments)}
+            gtag('js', new Date()); gtag('config', 'G-79B88YP1GN');
+          </script>
+        </Helmet>
         <Header />
         <Nav />
         <About />
@@ -22,11 +34,9 @@ const App = () => {
         <Testimonials />
         <Contact />
         <Footer />
-
-
       </>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
