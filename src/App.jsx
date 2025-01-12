e7e8d8e4d912b4430c3b930c30a7af829a096b93
Aditya Ranjan <aditya74810@gmail.com>
2025-01-12 17:42:44 +0530

    changes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
 import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
@@ -13,28 +16,29 @@ import { Helmet } from "react-helmet";
 const App = () => {
   return (
     <div>
-      <>
-        <Helmet>
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-79B88YP1GN"
-          ></script>
-          <script>
-            window.dataLayer = window.dataLayer || []; function gtag()
-            {dataLayer.push(arguments)}
-            gtag('js', new Date()); gtag('config', 'G-79B88YP1GN');
-          </script>
-        </Helmet>
-        <Header />
-        <Nav />
-        <About />
-        <Experience />
-        <Services />
-        <Portfolio />
-        <Testimonials />
-        <Contact />
-        <Footer />
-      </>
+      <Helmet>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-79B88YP1GN"
+        ></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-79B88YP1GN');
+          `}
+        </script>
+      </Helmet>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   );
 };
