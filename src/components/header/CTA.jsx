25f41dcdcf0dd9eb488c10351cfbd1659db0572e
Aditya Ranjan <aditya74810@gmail.com>
2025-04-08 12:44:54 +0530

    added a button

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CTA = () => {
     return (
         <div className="cta">
-            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
-            {/* <a href="#contact" className='btn btn-primary' > My Blog</a> */}
-
+            <a href="#contact" className="btn">Let's Talk</a>
+            <Link to="/blog" className="btn btn-primary">My Blog</Link>
         </div>
     )
 }
